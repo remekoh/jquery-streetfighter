9ea10c0009dcc47fec8f6ff3fc055bf7d477724e
remekoh <git@remekoh.me>
2015-02-18 22:37:48 -0600

    Added mousedown & mouseup functions, and also the hadouken sound

--- js/app.js
+++ js/app.js
@@ -7,4 +7,28 @@ $(document).ready(function() {
     $('.ryu-ready').hide();
 	$('.ryu-still').show();
   })
+  .mousedown(function() {
+  	playHadouken();
+  	$('.ryu-ready').hide();
+	$('.ryu-throwing').show();
+	$('.hadouken').finish().show()
+	  .animate(
+	  	{'left': '300px'},
+	      500,
+	  	  function() {
+	      	$(this).hide();
+	      	$(this).css('left', '-170px');
+  	      }
+	  );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
