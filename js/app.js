92b518c236c3cb6ba48931bfc99450582c83fb4c
remekoh <git@remekoh.me>
2015-02-19 19:58:07 -0600

    jQuery Challenge

--- js/app.js
+++ js/app.js
@@ -26,9 +26,20 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   });
 });
-
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+$(document).keydown(function(e) {
+  if (e.keyCode == 88) {
+	$('.ryu-still').hide();
+    $('.ryu-cool').show();
+  }
+ })
+ .keyup(function(e) {
+   if (e.keyCode == 88) {
+   	$('.ryu-cool').hide();
+	$('.ryu-still').show();
+   }
+});
